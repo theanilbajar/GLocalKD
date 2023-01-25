fde9f89f94de881ffde0a5bbffe865be622e523d
Anil Kumar <anil946233@gmail.com>
2023-01-25 14:56:48 +0530

    Add sce_loss

--- util.py
+++ util.py
@@ -2,6 +2,8 @@ import community
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+import torch
+import torch.nn.functional as F
 
 def node_iter(G):
     # if float(nx.__version__)<2.0:
@@ -16,5 +18,13 @@ def node_dict(G):
     #     node_dict = G.node
     return node_dict
 
+def sce_loss(x, y, alpha=3):
+    x = F.normalize(x, p=2, dim=-1)
+    y = F.normalize(y, p=2, dim=-1)
+
+    loss = (1 - (x * y).sum(dim=-1)).pow_(alpha)
+
+    loss = loss.mean()
+    return loss
 
 
